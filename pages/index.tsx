9cef0c3566cdcbc26f771823f653a9c993a9c5ad
JunHatanaka <nextseason6@gmail.com>
2022-01-01 23:53:15 +0900

    Delete: Delete to prisma test codes.

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,26 +2,10 @@ import { PrismaClient } from '@prisma/client'
 import { useEffect, useState } from 'react';
 import Form from '../components/Form';
 
-export const getServerSideProps = async ({ req }) => {
-  const prisma: PrismaClient = new PrismaClient()
 
-  const uniqueUser = await prisma.shopping.findMany()
-  // Date型でエラーが起きるので、一度stringifyしてからparse
-  const user = JSON.parse(JSON.stringify(uniqueUser));
-
-  return { props: { user } }
-}
-
-export default ({ user }) => {
-  useEffect(() => {
-    console.info(user);
-  }, [])
-
+export default () => {
   return (
     <>
-    <div> 
-      {/* {JSON.stringify(user)} */}
-    </div>
       <Form />
     </>
   )
