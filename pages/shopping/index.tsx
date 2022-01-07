0982b2971f3dbda9d0894b82bfe282700c08d66e
JunHatanaka <nextseason6@gmail.com>
2022-01-08 00:56:07 +0900

    Add: Added shoppingAll view in shopping/index.

--- pages/shopping/index.tsx
+++ pages/shopping/index.tsx
@@ -1,9 +1,31 @@
-// パスは/shopping
+import axios from "axios";
+import { useEffect, useState } from "react";
+import Moment from 'moment';
 
 export default () => {
+    const [shoppingAll, setShoppingAll] = useState([]);
+    Moment.locale('ja');
+
+    useEffect(() => {
+        async function fetchData() {
+            const response = await axios.get('/api/shopping');
+            setShoppingAll(response.data);
+            return response;
+        }
+        fetchData();
+    },[])
+
     return (
         <>
-            <div>ここに一覧表示する</div>
+            <div>
+                {
+                    shoppingAll.map(shopping => {
+                        return(
+                            <div key={shopping.id}>{Moment(shopping.date).format('YYYY-MM-DD')}, {shopping.price}, {shopping.product}, {shopping.shop}</div>
+                        )
+                    })
+                }
+            </div>
         </>
     )
 }
