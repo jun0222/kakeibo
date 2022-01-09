8fb909604741fe73488cd59e8a2be491346dbb95
JunHatanaka <nextseason6@gmail.com>
2022-01-09 10:54:16 +0900

    Add: Added filterYear, filterMonth.

--- pages/shopping/index.tsx
+++ pages/shopping/index.tsx
@@ -4,6 +4,8 @@ import Moment from 'moment';
 
 export default () => {
     const [shoppingAll, setShoppingAll] = useState([]);
+    const [filterYear, setFilterYear] = useState("");
+    const [filterMonth, setFilterMonth] = useState("");
     Moment.locale('ja');
 
     useEffect(() => {
@@ -17,6 +19,37 @@ export default () => {
 
     return (
         <>
+            <select
+                onChange={(e) => {setFilterYear(e.target.value)}}
+            >
+                <option value="2021">2021</option>
+                <option value="2022">2022</option>
+                <option value="2023">2023</option>
+                <option value="2024">2024</option>
+                <option value="2025">2025</option>
+                <option value="2026">2026</option>
+                <option value="2027">2027</option>
+                <option value="2028">2028</option>
+                <option value="2029">2029</option>
+            </select>
+
+            <select
+                onChange={(e) => {setFilterMonth(e.target.value)}}
+            >
+                <option value="01">01</option>
+                <option value="02">02</option>
+                <option value="03">03</option>
+                <option value="04">04</option>
+                <option value="05">05</option>
+                <option value="06">06</option>
+                <option value="07">07</option>
+                <option value="08">08</option>
+                <option value="09">09</option>
+                <option value="10">10</option>
+                <option value="11">11</option>
+                <option value="12">12</option>
+            </select>
+
             {shoppingAll.map(shopping => {
                 return(
                     <>
