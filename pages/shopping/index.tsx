e32ed6076e7c329eb3ce7f71e85be483b94d867d
JunHatanaka <nextseason6@gmail.com>
2022-01-12 22:50:37 +0900

    Add: Added react-chartjs-2 library && sample code.

--- pages/shopping/index.tsx
+++ pages/shopping/index.tsx
@@ -1,7 +1,28 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
 import Moment from 'moment';
+import React from "react";
+import {
+    Chart as ChartJS,
+    CategoryScale,
+    LinearScale,
+    PointElement,
+    LineElement,
+    Title,
+    Tooltip,
+    Legend,
+} from 'chart.js'
 
+import { Line } from 'react-chartjs-2'  
+ChartJS.register(
+    CategoryScale,
+    LinearScale,
+    PointElement,
+    LineElement,
+    Title,
+    Tooltip,
+    Legend
+)
 export default () => {
     const [shoppingAll, setShoppingAll] = useState([]);
     const [filterYYYY, setFilterYYYY] = useState(Moment(new Date()).format('YYYY'));
@@ -23,8 +44,45 @@ export default () => {
         setFilterYYYYMM(filter)
     },[filterYYYY, filterMM])
 
+    const labels = ["1 月", "2 月", "3 月", "4 月", "5 月", "6 月"];
+    const graphData = {
+        labels: labels,
+        datasets: [
+            {
+            label: "A社",
+            data: [65, 59, 60, 81, 56, 55],
+            borderColor: "rgb(75, 192, 192)",
+            },
+            {
+            label: "B社",
+            data: [60, 55, 57, 61, 75, 50],
+            borderColor: "rgb(75, 100, 192)",
+            },
+        ],
+    };
+    
+    const options: {} = {
+        maintainAspectRatio: false,
+    };
+    
+    const divStyle: React.CSSProperties = {
+        marginLeft: "auto",
+        marginRight: "auto",
+        margin: "10px",
+        width: "500px",
+    };
+
     return (
         <>
+            <div className="App" style={divStyle}>
+                <Line
+                    height={300}
+                    width={300}
+                    data={graphData}
+                    options={options}
+                    id="chart-key"
+                />
+            </div>
             <select
                 value={filterYYYY}
                 onChange={(e) => {setFilterYYYY(e.target.value)}}
