bb00e113c12ce0abc606763989fc7803f632770e
JunHatanaka <nextseason6@gmail.com>
2022-01-25 01:26:08 +0900

    Add: Added next-auth halfway.

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,24 @@
-import { useEffect, useState } from 'react';
+import { useSession, signIn, signOut } from "next-auth/react"
+import { SessionProvider } from "next-auth/react"
 import ShoppingIndex from './shopping';
 
-
 export default () => {
+  const { data: session } = useSession()
+  if (session) {
+    return (
+      <>
+        Signed in as {session.user.email} <br />
+        <button onClick={() => signOut()}>Sign out</button>
+      </>
+    )
+  }
   return (
     <>
-      <ShoppingIndex />
+      Not signed in <br />
+      <button onClick={() => signIn()}>Sign in</button>
+      <SessionProvider session={session}>
+        <ShoppingIndex />
+      </SessionProvider>
     </>
   )
 }
