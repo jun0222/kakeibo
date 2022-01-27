166d0326f0c5533060849faa3a48f62decc8cf21
JunHatanaka <nextseason6@gmail.com>
2022-01-28 02:12:10 +0900

    Add: Added next-auth halfway.

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -1,22 +1,15 @@
 // pages/api/auth/[...nextauth].js
 import NextAuth from "next-auth"
-import AppleProvider from "next-auth/providers/apple"
-import GoogleProvider from "next-auth/providers/google"
 import EmailProvider from "next-auth/providers/email"
+import { PrismaAdapter } from "@next-auth/prisma-adapter"
+import { PrismaClient } from "@prisma/client"
+
+const prisma = new PrismaClient()
 
 export default NextAuth({
+    adapter: PrismaAdapter(prisma),
     secret: process.env.SECRET,
     providers: [
-        // OAuth authentication providers
-        // AppleProvider({
-        // clientId: process.env.APPLE_ID,
-        // clientSecret: process.env.APPLE_SECRET,
-        // }),
-        // GoogleProvider({
-        // clientId: process.env.GOOGLE_ID,
-        // clientSecret: process.env.GOOGLE_SECRET,
-        // }),
-        // Sign in with passwordless email link
         EmailProvider({
             server: {
                 host: process.env.EMAIL_SERVER_HOST,
