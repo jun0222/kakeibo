d5f94bdb8113b3c439be8475afe737ba600b8f6c
JunHatanaka <nextseason6@gmail.com>
2022-01-29 01:39:15 +0900

    Add: Added next-auth complete.

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -1,30 +1,11 @@
-// pages/api/auth/[...nextauth].js
-import NextAuth from "next-auth"
-import EmailProvider from "next-auth/providers/email"
-import GoogleProvider from "next-auth/providers/google";
-import { PrismaAdapter } from "@next-auth/prisma-adapter"
-import { PrismaClient } from "@prisma/client"
-
-const prisma = new PrismaClient()
+import NextAuth from 'next-auth';
+import GoogleProvider from 'next-auth/providers/google';
 
 export default NextAuth({
-    adapter: PrismaAdapter(prisma),
-    secret: process.env.SECRET,
     providers: [
         GoogleProvider({
-            clientId: process.env.GOOGLE_CLIENT_ID,
-            clientSecret: process.env.GOOGLE_CLIENT_SECRET
-        }),
-        EmailProvider({
-            server: {
-                host: process.env.EMAIL_SERVER_HOST,
-                port: process.env.EMAIL_SERVER_PORT,
-                auth: {
-                user: process.env.EMAIL_SERVER_USER,
-                pass: process.env.EMAIL_SERVER_PASSWORD
-            }
-            },
-            from: process.env.EMAIL_FROM
+        clientId: process.env.GOOGLE_CLIENT_ID,
+        clientSecret: process.env.GOOGLE_CLIENT_SECRET,
         }),
     ],
-})
+});
