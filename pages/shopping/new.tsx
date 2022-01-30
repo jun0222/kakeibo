8c0cb5726de2f5f30c4da6a4098d0b8a89ef7c2e
JunHatanaka <nextseason6@gmail.com>
2022-01-30 17:43:02 +0900

    Add: Added signin check to pages/shopping/new .

--- pages/shopping/new.tsx
+++ pages/shopping/new.tsx
@@ -1,9 +1,19 @@
+import { useSession } from "next-auth/react" // https://next-auth.js.org/
 import NewShoppingForm from '../../components/NewShoppingForm';
+import SigninForm from '../../components/SigninForm';
 
 export default () => {
+    const { data: session } = useSession()
+    if (session) {
+        return (
+            <>
+                <NewShoppingForm />
+            </>
+        )
+    }
     return (
         <>
-            <NewShoppingForm />
+            <SigninForm />
         </>
     )
 }
