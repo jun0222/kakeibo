a68295ab7efc119e209eacb47e7564706e8967b4
JunHatanaka <nextseason6@gmail.com>
2022-01-30 19:23:31 +0900

    Add: Added NextLink.

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,7 @@
 import { useSession, signIn, signOut } from "next-auth/react" // https://next-auth.js.org/
 import SigninForm from "../components/SigninForm";
 import ShoppingIndex from './shopping';
+import Link from 'next/link'
 
 export default () => {
   const { data: session } = useSession()
@@ -9,6 +10,9 @@ export default () => {
       <>
         Signed in as {session.user.email} <br />
         <button onClick={() => signOut()}>Sign out</button>
+        <Link href="/shopping/new">
+            <a>入力画面へ</a>
+        </Link>
         <ShoppingIndex />
       </>
     )
--- pages/shopping/new.tsx
+++ pages/shopping/new.tsx
@@ -1,12 +1,16 @@
 import { useSession } from "next-auth/react" // https://next-auth.js.org/
 import NewShoppingForm from '../../components/NewShoppingForm';
 import SigninForm from '../../components/SigninForm';
+import Link from 'next/link';
 
 export default () => {
     const { data: session } = useSession()
     if (session) {
         return (
             <>
+                <Link href="/">
+                    <a>家計簿画面へ</a>
+                </Link>
                 <NewShoppingForm />
             </>
         )
