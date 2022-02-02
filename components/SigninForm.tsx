e728ef91b075fc166bfd944d214f5a0b5453cd55
JunHatanaka <nextseason6@gmail.com>
2022-02-03 02:29:44 +0900

    Add: Added button to demo-mode.

--- components/SigninForm.tsx
+++ components/SigninForm.tsx
@@ -4,8 +4,11 @@ export default function SigninForm() {
     return(
         <div className="flex items-center min-h-screen">
             <div className="container mx-auto">
+                <div className="text-center mb-2">
+                    <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-6 rounded" onClick={() => signIn()}>Sign in</button>
+                </div>
                 <div className="text-center">
-                    <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" onClick={() => signIn()}>Sign in</button>
+                    <button className="bg-gray-500 hover:bg-gray-700 text-white font-bold py-2 px-6 rounded">demo</button>
                 </div>
             </div>
         </div>
