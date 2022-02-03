e8825f247e9ede7fc771ef7ffa6333cb25609810
JunHatanaka <nextseason6@gmail.com>
2022-02-04 00:51:36 +0900

    Add: Added DemoContext.tsx to make demo-mode.

---
+++ components/DemoContext.tsx
@@ -0,0 +1,22 @@
+// https://reffect.co.jp/react/react-usecontext-understanding
+// useContext利用でdemoモードを実装する
+import { createContext, useState, useContext } from 'react';
+
+const CountContext = createContext();
+
+export function useCountContext() {
+    return useContext(CountContext);
+}
+
+export function CountProvider({ children }) {
+    const [count, setCount] = useState(100);
+
+    const value = {
+        count,
+        setCount,
+    };
+
+    return (
+        <CountContext.Provider value={value}>{children}</CountContext.Provider>
+    );
+}
