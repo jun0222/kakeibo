280acac8ea5bf08627a3ca2773c5f0803b19f965
JunHatanaka <nextseason6@gmail.com>
2022-02-11 01:19:07 +0900

    Modify: Modified UI SigninForm.tsx .

--- components/SigninForm.tsx
+++ components/SigninForm.tsx
@@ -11,10 +11,10 @@ export default function SigninForm() {
             <div className="flex items-center min-h-screen">
                 <div className="container mx-auto">
                     <div className="text-center mb-2">
-                        <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-6 rounded" onClick={() => signIn()}>Sign in</button>
+                        <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-6 rounded" onClick={() => signIn()}>ログイン</button>
                     </div>
                     <div className="text-center">
-                        <button className="bg-gray-500 hover:bg-gray-700 text-white font-bold py-2 px-6 rounded" onClick={() => setDemoMode(true)}>demo</button>
+                        <button className="bg-gray-500 hover:bg-gray-700 text-white font-bold py-2 px-6 rounded" onClick={() => setDemoMode(true)}>Demo</button>
                     </div>
                 </div>
             </div>
