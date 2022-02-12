3d996192a2a2707097112577c4c7ffb93604735d
JunHatanaka <nextseason6@gmail.com>
2022-02-13 02:38:28 +0900

    Add: Added delete api test halfway Until you can use the import statement.

--- shoppingDelete.test.js
+++ shoppingDelete.test.js
@@ -1,5 +1,14 @@
+/*
+    状況：importを使ったテストが通った、
+    しかし、axios.deleteでdb内容を実際には削除できない
+    expect(res).toBeTruthy();なので通っているだけ
+*/
+
 // https://qiita.com/chimame/items/e97883fd46b67529d59f jestの文法
 // https://qiita.com/irico/items/0fa0cde39b1305c4b508 jestでimport文(es6)を使う方法 途中まで、jest: command not foundになっている
+// https://jestjs.io/ja/docs/getting-started babel-jestの使い方
+// https://stackoverflow.com/questions/69467016/parsing-error-cannot-find-module-babel-preset-react Cannot find module '@babel/preset-react'
+// https://qiita.com/riversun/items/63c5f08c8da604c5ec1a Cannot find module 'core-js/modules/es.object.to-string.js'
 import { PrismaClient } from "@prisma/client";
 import axios from "axios";
 const prisma = new PrismaClient();
