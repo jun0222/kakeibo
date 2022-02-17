127ee6f4c9a20dd922eee9f84f18fbb46426c0fc
JunHatanaka <nextseason6@gmail.com>
2022-02-18 00:09:23 +0900

    Add: demoMode to use localstrage comment.

--- components/DemoContext.tsx
+++ components/DemoContext.tsx
@@ -1,5 +1,6 @@
 // https://reffect.co.jp/react/react-usecontext-understanding
 // useContext利用でdemoモードを実装する
+// shoppingの登録や削除などでリロードしても保持されるよう、localstrageに置き換え予定 https://qiita.com/Ryusou/items/8bce84e7b036114b8d72
 
 // @ts-nocheck // 一旦デプロイのためエラー無視。後で直す。
 import { createContext, useState, useContext } from 'react';
