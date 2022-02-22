4dffc664a65841db4ac845f8404e218e9f24c5b7
JunHatanaka <nextseason6@gmail.com>
2022-02-23 00:35:18 +0900

    Change: Changed get from now foodExpensesPaidThisMonth.

--- pages/api/linebot.ts
+++ pages/api/linebot.ts
@@ -8,13 +8,21 @@ export default async (req: NextApiRequest, res: NextApiResponse) => {
     // 今月の食費利用額
     // →今月はDate newなど？で取得し、
     // 　DBから取得した今月のshoppingのprice合計値を出す。
+
+    // 今月のYYYYとDDを取得
+    const nowDate = new Date()
+    const toYear = nowDate.getFullYear().toString();
+    const toMonth = nowDate.getMonth() + 1;
+    const toMonthZeroPadding =  ( '00' + toMonth ).slice( -2 );
+
     const shoppings = await prisma.shopping.findMany({
         where: {
             userId: "jundev63@gmail.com", // ハードコーディングしているので、該当ユーザーのメールアドレスを使うようにするLINE連携画面が必要？
-            // 今月のものに限定する、今は2月のとしているが、当月の01~31日とする。
+
+            // 月初,月末がうまく取れなかったので今月の01日〜31日を取得
             date: {
-                gte: new Date("2022-02-01"),
-                lt:  new Date("2022-02-31")
+                gte: new Date(toYear+`-`+toMonthZeroPadding+`-01`),
+                lt:  new Date(toYear+`-`+toMonthZeroPadding+`-31`)
             }
         }
     });
